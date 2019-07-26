25e10ffb2a968a5cef7644ce404c4674446e6c28
CirnoV <cirnov59@gmail.com>
2019-07-26 22:43:17 +0900

    Add watermark

--- src/pixi-component/CheatSheet.tsx
+++ src/pixi-component/CheatSheet.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Stage, Container } from '@inlet/react-pixi';
+import { Stage, Container, Text } from '@inlet/react-pixi';
 import ShipContainer from './ShipContainer';
 import { KanmusuList } from '../lib/utils';
 import { sheetData } from '../lib/sheetData';
+import { TextStyle } from 'pixi.js';
 
 type CheatSheetProps = {
   kanmusu: KanmusuList;
@@ -16,37 +17,49 @@ const CheatSheet: React.FC<CheatSheetProps> = ({ kanmusu }) => (
     options={{ backgroundColor: 0xffffff, preserveDrawingBuffer: true }}
   >
     <Container>
-      <ShipContainer
-        name="- 戦艦 -"
-        kanmusu={kanmusu}
-        sheetData={sheetData.BB}
-      />
-      <ShipContainer
-        y={700}
-        name="- 空母 -"
-        kanmusu={kanmusu}
-        sheetData={sheetData.CV}
-      />
+      <Container>
+        <ShipContainer
+          name="- 戦艦 -"
+          kanmusu={kanmusu}
+          sheetData={sheetData.BB}
+        />
+        <ShipContainer
+          y={700}
+          name="- 空母 -"
+          kanmusu={kanmusu}
+          sheetData={sheetData.CV}
+        />
+      </Container>
+      <Container x={700}>
+        <ShipContainer
+          name="- 重巡 -"
+          kanmusu={kanmusu}
+          sheetData={sheetData.CA}
+        />
+        <ShipContainer
+          y={400}
+          name="- 軽巡 -"
+          kanmusu={kanmusu}
+          sheetData={sheetData.CL}
+        />
+        <ShipContainer
+          y={680}
+          name="- 駆逐 -"
+          kanmusu={kanmusu}
+          sheetData={sheetData.DD}
+        />
+      </Container>
     </Container>
-    <Container x={700}>
-      <ShipContainer
-        name="- 重巡 -"
-        kanmusu={kanmusu}
-        sheetData={sheetData.CA}
-      />
-      <ShipContainer
-        y={400}
-        name="- 軽巡 -"
-        kanmusu={kanmusu}
-        sheetData={sheetData.CL}
-      />
-      <ShipContainer
-        y={680}
-        name="- 駆逐 -"
-        kanmusu={kanmusu}
-        sheetData={sheetData.DD}
-      />
-    </Container>
+    <Text
+      x={4}
+      y={1478}
+      text="https://icicle.moe/blueprint-cheatsheet"
+      style={
+        new TextStyle({
+          fontSize: 18
+        })
+      }
+    />
   </Stage>
 );
 
